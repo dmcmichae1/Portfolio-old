21c98de75e3e3d2dc846ae7a5afb9cdf587e15f8
dmcmichae1 <mcmichaeldevin@icloud.com>
2021-08-14 20:16:09 -0700

    added alot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  //Switch,
+  Route,
+  //useParams
+} from "react-router-dom";
+import Header from './Components/Header';
+import Showcase from './Components/Showcase';
+import Projects from './Components/Projects';
+import AboutMe from './Components/AboutMe';
+import Skills from './Components/Skills';
+import Personality from './Components/Personality';
+
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Header />
+
+      <Route path="/" exact>
+        <Showcase />
+      </Route>
+
+      <Route path="/projects">
+        <Projects />
+      </Route >
+
+      <Route path="/aboutme">
+        <AboutMe />
+      </Route>
+
+      <Route path="/skills">
+        <Skills />
+      </Route>
+
+      <Route path="/personality">
+        <Personality />
+      </Route>
+
+    </Router>
   );
 }
 
